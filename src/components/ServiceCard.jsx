7420e007722461b5217e20d8b1622f4d29b8202c
sparky0520 <darshjain05.in@gmail.com>
2025-09-04 11:46:25 +0530

    Security headers added from lighthouse report

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -2,7 +2,6 @@ function ServiceCard({ service }) {
   return (
     <div className="flex justify-between gap-12 border-[1px] card">
       <div>{service.name}</div>
-      <div></div>
     </div>
   );
 }
